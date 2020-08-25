c49c05742bfba24d8e340bbb8f5f91fc798f0149
ZYXS <37678999+yxspring@users.noreply.github.com>
2020-08-25 17:32:39 +0800

    其他的相关文件

---
+++ 精通Spring4.x企业级应用开发实战学习文件夹/spri/src/main/java/com/yxs/spri/SpriApplication.java
@@ -0,0 +1,13 @@
+package com.yxs.spri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpriApplication.class, args);
+    }
+
+}
---
+++ 精通Spring4.x企业级应用开发实战学习文件夹/spri/src/test/java/com/yxs/spri/SpriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxs.spri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
